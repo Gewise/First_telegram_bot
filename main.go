d07a5f8aa1569f08cc60272a9b9bea28a367418a
GM <george.malygin25@yandex.ru>
2024-03-23 22:38:04 +0300

    third Create simple echo bot

--- main.go
+++ main.go
@@ -1,13 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	txt := "privet mir"
-	txt2 := "uwu"
-	txt3 := "GWS"
-	txt4 := "(*_*)"
 
-	fmt.Println(txt, txt2, txt3, txt4)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
 
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	token, ok := os.LookupEnv("token")
+	if !ok {
+		panic("Token is not found")
+	}
+
+	b, err := bot.New(token, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   "Привет, " + update.Message.Text,
+	})
 }
